58f0c21402817b0cd4198045286a72ac7f14cb08
eensander <50321538+eensander@users.noreply.github.com>
2025-04-15 16:12:16 +0200

    add and move tests

--- tests/exact/test_bw.py
+++ tests/exact/test_bw.py
@@ -4,15 +4,15 @@ from src import search
 # "Art. 5:1 BW",
 # "Art. 5:1 lid 2 BW",
 
-# def test_no_art():
-#     results = search.query_exact("5:1 BW")
+def test_no_art():
+    results = search.query_exact("5:1 BW")
+    
+    assert len(results) == 1, "should be one result"
+    result = results[0]
 
-#     assert len(results) == 1, "should be one result"
-#     result = results[0]
-
-#     assert result['resource']['id'] == 'BWBR0005288', "should be identifier for BW book 5"
-#     assert result['fragment']['article'] == '1', "should be article number 1"
-#     assert result['fragment']['book'] == '5', "should be book number 5"
+    assert result['resource']['id'] == 'BWBR0005288', "should be identifier for BW book 5"
+    assert result['fragment']['article'] == '1', "should be article number 1"
+    assert result['fragment']['book'] == '5', "should be book number 5"
 
 def test_art_simple():
     results = search.query_exact("Art. 5:1 BW")
